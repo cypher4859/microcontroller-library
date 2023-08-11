592d8851e9aa1c204f91ddd5afb40aa61f869a19
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2023-08-10 21:44:42 -0400

    Setting up the module structure for the microcontroller library

---
+++ packages/vibration_sensor/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='vibration_sensor',
+    version='0.1.0',    
+    description='Module for handling detection of vibration and reacting.',
+    author='cypher4859',
+    packages=['vibration_sensor_node'],
+)
