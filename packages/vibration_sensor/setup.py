eaf6414d0fb810b179bafea4b96f8db80b18d0af
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2023-08-10 21:57:22 -0400

    Got the setup py working correctly

--- packages/vibration_sensor/setup.py
+++ packages/vibration_sensor/setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='vibration_sensor',
     version='0.1.0',    
     description='Module for handling detection of vibration and reacting.',
     author='cypher4859',
-    packages=['vibration_sensor_node'],
+    package_dir={"": "vibration_sensor_node"},
+    packages=find_packages("vibration_sensor_node"),
 )
